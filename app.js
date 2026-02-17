b05732da753bbb5e4d2db0af84fe65bc40269978
AnshMadanpuriya <anshmadanpuriya16@gmail.com>
2026-02-18 00:05:45 +0530

    Add Project Files

---
+++ app.js
@@ -0,0 +1,104 @@
+if(process.env.NODE_ENV != "production"){
+  require("dotenv").config(); 
+}
+
+console.log(process.env.SECRET);
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");  //for ejs:
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+const dbUrl = process.env.ATLASDB_URL;
+
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(dbUrl);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+const store = MongoStore.create({
+    mongoUrl: process.env.ATLASDB_URL, // get from .env 
+    crypto: {
+        secret: process.env.SECRET,     
+    },
+    touchAfter: 24 * 3600 // 24 hours
+});
+store.on("error", (err) => {
+    console.log("SESSION STORE ERROR", err);
+});
+
+const sessionOptions = {
+    store: store,
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000
+    }
+};
+
+app.use(session(sessionOptions));
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).send(message);
+});
+
+
+app.listen(8080, () => {
+  console.log("Server is Running to port 8080");
+});
